97ed90fccb7a91fe9e5e3eb85d9f082bd114edff
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-04 08:47:14 +0200

    added di package and Singleton Module

---
+++ app/src/main/java/com/example/limbushelper/di/Singleton.kt
@@ -0,0 +1,10 @@
+package com.example.limbushelper.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Singleton {
+}
