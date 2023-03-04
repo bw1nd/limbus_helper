7e6a2318c34836af19088ec9b0743c00c4b6cab0
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-04 16:18:31 +0200

    added party repository implementation

---
+++ app/src/main/java/com/example/limbushelper/data/PartyRepository.kt
@@ -0,0 +1,37 @@
+package com.example.limbushelper.data
+
+import com.example.limbushelper.data.db.Database
+import com.example.limbushelper.data.db.model.PartyEntity
+import com.example.limbushelper.data.db.model.toPartyIdentityPair
+import com.example.limbushelper.domain.party.IPartyRepository
+import com.example.limbushelper.domain.party.model.Party
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class PartyRepository @Inject constructor(
+    db: Database
+): IPartyRepository {
+    private val dao = db.dao
+
+    override fun getParty(): Flow<Party> {
+        return dao.getParty().map { list ->
+            Party(PLACEHOLDER_PARTY_ID, list.map { it.toPartyIdentityPair() })
+        }
+    }
+
+    /**
+     * this method is used for both adding new identities and changing status of old with replace.
+     */
+    override suspend fun addIdentityToParty(identity: Pair<Int, Boolean>) {
+        dao.addIdentityToParty(PartyEntity(identity.first, identity.second))
+    }
+
+    override suspend fun deleteIdentityFromParty(identity: Pair<Int, Boolean>) {
+        dao.deleteIdentityFromParty(PartyEntity(identity.first, identity.second))
+    }
+
+    companion object {
+        private const val PLACEHOLDER_PARTY_ID = 0
+    }
+}
