c053e4ad326a0787d3d587ee5ca7805c7950fbc4
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-05 17:47:43 +0200

    added sin colors

--- app/src/main/java/ua/blackwind/limbushelper/ui/theme/Color.kt
+++ app/src/main/java/ua/blackwind/limbushelper/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val wrath  = Color(0xFFA0392B)
+val lust = Color(0xFFBB521F)
+val sloth = Color(0xFFDC8401)
+val gluttony = Color(0xFF5E7D28)
+val gloom = Color(0xFF2C5D68)
+val pride = Color(0xFF15497B)
+val envy = Color(0xFF744888)
+
