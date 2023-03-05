fe87d508b9eceecc547665ac8e791232f3d7ece5
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-05 16:22:25 +0200

    added viewmodel to filter screen

--- app/src/main/java/ua/blackwind/limbushelper/ui/screens/filter_screen/FilterScreen.kt
+++ app/src/main/java/ua/blackwind/limbushelper/ui/screens/filter_screen/FilterScreen.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
+import ua.blackwind.limbushelper.domain.sinner.model.Identity
 import ua.blackwind.limbushelper.ui.common.IdentityItem
 import ua.blackwind.limbushelper.ui.previewIdentity
 
@@ -18,21 +21,15 @@ import ua.blackwind.limbushelper.ui.previewIdentity
 @Destination
 @Composable
 fun FilterScreen() {
+    val viewModel = hiltViewModel<FilterScreenViewModel>()
+    val identities = viewModel.filteredIdentities.collectAsState()
     FilterScreenUi(
-        identities =
-        listOf(
-            previewIdentity,
-            previewIdentity,
-            previewIdentity,
-            previewIdentity,
-            previewIdentity,
-
-            )
+        identities.value
     )
 }
 
 @Composable
-fun FilterScreenUi(identities: List<ua.blackwind.limbushelper.domain.sinner.model.Identity>) {
+fun FilterScreenUi(identities: List<Identity>) {
     Surface(modifier = Modifier.fillMaxSize()) {
         LazyColumn(
             contentPadding = PaddingValues(5.dp),
