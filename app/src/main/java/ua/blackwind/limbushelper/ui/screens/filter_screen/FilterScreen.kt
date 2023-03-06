890b5fc2511f26444c3e3026696207229858d7c1
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-06 19:22:57 +0200

    implemented base filter composables

--- app/src/main/java/ua/blackwind/limbushelper/ui/screens/filter_screen/FilterScreen.kt
+++ app/src/main/java/ua/blackwind/limbushelper/ui/screens/filter_screen/FilterScreen.kt
@@ -1,10 +1,6 @@
 package ua.blackwind.limbushelper.ui.screens.filter_screen
 
-import android.widget.ToggleButton
 import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
@@ -12,13 +8,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
@@ -28,7 +23,6 @@ import ua.blackwind.limbushelper.domain.DamageType
 import ua.blackwind.limbushelper.domain.Sin
 import ua.blackwind.limbushelper.domain.sinner.model.Identity
 import ua.blackwind.limbushelper.ui.common.IdentityItem
-import ua.blackwind.limbushelper.ui.previewIdentity
 import ua.blackwind.limbushelper.ui.util.getDamageTypeIcon
 import ua.blackwind.limbushelper.ui.util.getSinColor
 
@@ -38,13 +32,30 @@ import ua.blackwind.limbushelper.ui.util.getSinColor
 fun FilterScreen() {
     val viewModel = hiltViewModel<FilterScreenViewModel>()
     val identities = viewModel.filteredIdentities.collectAsState()
-    FilterScreenUi(
-        identities.value
+    val filterSkillState = viewModel.filterSkillsState.collectAsState()
+    val filterResistState = viewModel.filterResistState.collectAsState()
+    val labels = FilterResistButtonLabels(
+        stringResource(R.string.res_ineff),
+        stringResource(R.string.res_normal),
+        stringResource(R.string.res_fatal)
     )
+
+//    FilterScreenUi(
+//        identities.value
+//    )
 }
 
 @Composable
-fun FilterScreenUi(identities: List<Identity>) {
+fun FilterScreenUi(
+    identities: List<Identity>,
+    skillState: FilterSkillBlockState,
+    resistState: FilterResistBlockState,
+    resistLabels: FilterResistButtonLabels,
+    onSwitchChange: (Boolean) -> Unit,
+    onSkillButtonClick: (Int) -> Unit,
+    onResistButtonClick: (Int) -> Unit
+
+) {
     Box(contentAlignment = Alignment.BottomCenter) {
         Surface(modifier = Modifier.fillMaxSize()) {
             LazyColumn(
@@ -58,12 +69,26 @@ fun FilterScreenUi(identities: List<Identity>) {
                 }
             }
         }
-        FilterDrawerSheet()
+        FilterDrawerSheet(
+            skillState = skillState,
+            resistState = resistState,
+            resistLabels = resistLabels,
+            onSwitchChange = onSwitchChange,
+            onSkillButtonClick = onSkillButtonClick,
+            onResistButtonClick = onResistButtonClick
+        )
     }
 }
 
 @Composable
-fun FilterDrawerSheet() {
+fun FilterDrawerSheet(
+    skillState: FilterSkillBlockState,
+    resistState: FilterResistBlockState,
+    resistLabels: FilterResistButtonLabels,
+    onSwitchChange: (Boolean) -> Unit,
+    onSkillButtonClick: (Int) -> Unit,
+    onResistButtonClick: (Int) -> Unit
+) {
     var drawerState by remember { mutableStateOf<FilterDrawerState>(FilterDrawerState.Open) }
     var height by remember { mutableStateOf(200.dp) }
 
@@ -89,34 +114,44 @@ fun FilterDrawerSheet() {
                     contentDescription = null
                 )
             }
-            Text("there will be our filter")
+            FilterBlock(
+                skillState = skillState,
+                resistState = resistState,
+                resistLabels = resistLabels,
+                onSwitchChange = onSwitchChange,
+                onSkillButtonClick = onSkillButtonClick,
+                onResistButtonClick = onResistButtonClick
+            )
         }
     }
-
 }
 
 @Composable
 fun FilterBlock(
-    skillState: FilterSkillBlockButtonsState,
-    onSwitchChange: () -> Unit,
-    onSkillButtonClick: (Int) -> Unit
+    skillState: FilterSkillBlockState,
+    resistState: FilterResistBlockState,
+    resistLabels: FilterResistButtonLabels,
+    onSwitchChange: (Boolean) -> Unit,
+    onSkillButtonClick: (Int) -> Unit,
+    onResistButtonClick: (Int) -> Unit
 ) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Text(text = "Skills/Resistances")
             Spacer(modifier = Modifier.width(10.dp))
-            Switch(checked = false, onCheckedChange = { onSwitchChange() })
+            Switch(checked = false, onCheckedChange = onSwitchChange)
             Spacer(modifier = Modifier.width(10.dp))
             Text(text = "Effects")
         }
         FilterSkillBlock(state = skillState, onButtonClick = onSkillButtonClick)
         Divider(thickness = 2.dp, color = MaterialTheme.colorScheme.primary)
+        FilterResistBlock(labels = resistLabels, state = resistState, onResistButtonClick)
 
     }
 }
 
 @Composable
-fun FilterSkillBlock(state: FilterSkillBlockButtonsState, onButtonClick: (Int) -> Unit) {
+fun FilterSkillBlock(state: FilterSkillBlockState, onButtonClick: (Int) -> Unit) {
     Row() {
         FilterSkillButton(id = 1, state = state.first, onClick = onButtonClick)
         FilterSkillButton(id = 2, state = state.second, onClick = onButtonClick)
@@ -124,14 +159,14 @@ fun FilterSkillBlock(state: FilterSkillBlockButtonsState, onButtonClick: (Int) -
     }
 }
 
-data class FilterSkillBlockButtonsState(
-    val first: FilterSkillState,
-    val second: FilterSkillState,
-    val third: FilterSkillState
+data class FilterSkillBlockState(
+    val first: FilterSkillButtonState,
+    val second: FilterSkillButtonState,
+    val third: FilterSkillButtonState
 )
 
 @Composable
-fun FilterSkillButton(id: Int, state: FilterSkillState, onClick: (Int) -> Unit) {
+fun FilterSkillButton(id: Int, state: FilterSkillButtonState, onClick: (Int) -> Unit) {
     Surface(
         shape = CircleShape,
         color = state.sin?.let { getSinColor(it) } ?: Color.White
@@ -147,7 +182,59 @@ fun FilterSkillButton(id: Int, state: FilterSkillState, onClick: (Int) -> Unit) 
     }
 }
 
-data class FilterSkillState(val type: DamageType?, val sin: Sin?)
+@Composable
+fun FilterResistBlock(
+    labels: FilterResistButtonLabels, state: FilterResistBlockState,
+    onButtonClick: (Int) -> Unit
+) {
+    Row() {
+        FilterResistButton(
+            1,
+            label = labels.ineffective,
+            state = state.ineffective,
+            onClick = onButtonClick
+        )
+        FilterResistButton(
+            2,
+            label = labels.normal,
+            state = state.normal,
+            onClick = onButtonClick
+        )
+        FilterResistButton(
+            3,
+            label = labels.fatal,
+            state = state.fatal,
+            onClick = onButtonClick
+        )
+    }
+}
+
+@Composable
+fun FilterResistButton(id: Int, label: String, state: DamageType?, onClick: (Int) -> Unit) {
+    Column() {
+        IconButton(onClick = { onClick(id) }) {
+            Icon(painter = painterResource(id = state?.let {
+                getDamageTypeIcon(it)
+            } ?: R.drawable.charge_ic
+            ), contentDescription = null)
+        }
+        Text(text = label)
+    }
+}
+
+data class FilterResistBlockState(
+    val ineffective: DamageType?,
+    val normal: DamageType?,
+    val fatal: DamageType?
+)
+
+data class FilterResistButtonLabels(
+    val ineffective: String,
+    val normal: String,
+    val fatal: String
+)
+
+data class FilterSkillButtonState(val type: DamageType?, val sin: Sin?)
 
 private sealed class FilterDrawerState() {
     object Open: FilterDrawerState()
@@ -158,12 +245,14 @@ private sealed class FilterDrawerState() {
 @Composable
 fun PreviewFilterBlock() {
     FilterBlock(
-        FilterSkillBlockButtonsState(
-            FilterSkillState(DamageType.BLUNT, Sin.LUST),
-            FilterSkillState(DamageType.SLASH, null),
-            FilterSkillState(null, null)
+        FilterSkillBlockState(
+            FilterSkillButtonState(DamageType.BLUNT, Sin.LUST),
+            FilterSkillButtonState(DamageType.SLASH, null),
+            FilterSkillButtonState(null, null)
         ),
-        {}, {}
+        FilterResistBlockState(null, DamageType.BLUNT, null),
+        FilterResistButtonLabels("Ineff.", "Normal", "Fatal"),
+        {}, {}, {}
     )
 }
 
