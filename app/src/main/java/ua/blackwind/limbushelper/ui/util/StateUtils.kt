3e829ccee69ae724c3cc6bd5a70a787c00b810bf
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-07 15:07:18 +0200

    commented filter state classes

--- app/src/main/java/ua/blackwind/limbushelper/ui/util/StateUtils.kt
+++ app/src/main/java/ua/blackwind/limbushelper/ui/util/StateUtils.kt
@@ -3,22 +3,35 @@ package ua.blackwind.limbushelper.ui.util
 import ua.blackwind.limbushelper.domain.DamageType
 import ua.blackwind.limbushelper.domain.Sin
 
+/**
+ * State holder representing single Damage or Sin value
+ */
 sealed class StateType<out T: Any> {
     object Empty: StateType<Nothing>()
     data class Value<T: Any>(val value: T): StateType<T>()
 }
 
+/**
+ * State holder representing state of skill filter block.
+ */
 data class FilterSkillBlockState(
     val damage: FilterDamageStateBundle,
     val sin: FilterSinStateBundle
 )
 
+/**
+ * State holder representing damage type state of skills or resistances
+ * in order they appear on screen.
+ */
 data class FilterDamageStateBundle(
     val first: StateType<DamageType>,
     val second: StateType<DamageType>,
     val third: StateType<DamageType>,
 )
-
+/**
+ * State holder representing sin type state of skills
+ * in order they appear on screen.
+ */
 data class FilterSinStateBundle(
     val first: StateType<Sin>,
     val second: StateType<Sin>,
