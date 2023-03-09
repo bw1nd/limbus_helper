1551b5a91bb48b7c5a2d55ba8a1a391a1859e6ea
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-09 21:47:07 +0200

    more skill tests a lot more..

--- app/src/test/java/ua/blackwind/limbushelper/domain/sinner/usecase/GetFilteredIdentitiesUseCaseTest.kt
+++ app/src/test/java/ua/blackwind/limbushelper/domain/sinner/usecase/GetFilteredIdentitiesUseCaseTest.kt
@@ -1,10 +1,10 @@
 package ua.blackwind.limbushelper.domain.sinner.usecase
 
-
 import io.mockk.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import ua.blackwind.limbushelper.data.SinnerRepository
 import ua.blackwind.limbushelper.domain.DamageType
@@ -23,76 +23,320 @@ class GetFilteredIdentitiesUseCaseTest {
 
     private val dummySupport = mockk<Support>()
 
-    private val testData = listOf(
-        generateIdentity(
-            "TEST IDENTITY #1",
-            IdentityDamageResistType.INEFFECTIVE,
-            IdentityDamageResistType.NORMAL,
-            IdentityDamageResistType.FATAL,
-            generateSkill(DamageType.SLASH, Sin.LUST, listOf(Effect.BURN)),
-            generateSkill(DamageType.SLASH, Sin.ENVY, listOf(Effect.BLEED)),
-            generateSkill(DamageType.BLUNT, Sin.WRATH, emptyList()),
-            dummyPassive,
-            dummySupport
-        ),
-        generateIdentity(
-            "TEST IDENTITY #2",
-            IdentityDamageResistType.NORMAL,
-            IdentityDamageResistType.INEFFECTIVE,
-            IdentityDamageResistType.FATAL,
-            generateSkill(DamageType.PIERCE, Sin.PRIDE, listOf(Effect.POISE)),
-            generateSkill(DamageType.PIERCE, Sin.PRIDE, listOf(Effect.POISE)),
-            generateSkill(DamageType.PIERCE, Sin.PRIDE, emptyList()),
-            dummyPassive,
-            dummySupport
-        ),
-        generateIdentity(
-            "TEST IDENTITY #3",
-            IdentityDamageResistType.NORMAL,
-            IdentityDamageResistType.FATAL,
-            IdentityDamageResistType.INEFFECTIVE,
-            generateSkill(DamageType.SLASH, Sin.GLUTTONY, listOf(Effect.POISE)),
-            generateSkill(DamageType.BLUNT, Sin.GLOOM, listOf(Effect.POISE)),
-            generateSkill(DamageType.BLUNT, Sin.LUST, emptyList()),
-            dummyPassive,
-            dummySupport
-        ),
+    private val repository = mockk<SinnerRepository>()
+
+    private val firstIdentity = generateIdentity(
+        "TEST IDENTITY #1",
+        IdentityDamageResistType.INEFFECTIVE,
+        IdentityDamageResistType.NORMAL,
+        IdentityDamageResistType.FATAL,
+        generateSkill(DamageType.SLASH, Sin.LUST, listOf(Effect.BURN)),
+        generateSkill(DamageType.SLASH, Sin.ENVY, listOf(Effect.BLEED)),
+        generateSkill(DamageType.BLUNT, Sin.WRATH, emptyList()),
+        dummyPassive,
+        dummySupport
+    )
+    private val secondIdentity = generateIdentity(
+        "TEST IDENTITY #2",
+        IdentityDamageResistType.NORMAL,
+        IdentityDamageResistType.INEFFECTIVE,
+        IdentityDamageResistType.FATAL,
+        generateSkill(DamageType.PIERCE, Sin.PRIDE, listOf(Effect.POISE)),
+        generateSkill(DamageType.PIERCE, Sin.SLOTH, listOf(Effect.POISE)),
+        generateSkill(DamageType.PIERCE, Sin.ENVY, emptyList()),
+        dummyPassive,
+        dummySupport
     )
 
+    private val thirdIdentity = generateIdentity(
+        "TEST IDENTITY #3",
+        IdentityDamageResistType.NORMAL,
+        IdentityDamageResistType.FATAL,
+        IdentityDamageResistType.INEFFECTIVE,
+        generateSkill(DamageType.SLASH, Sin.GLUTTONY, listOf(Effect.POISE)),
+        generateSkill(DamageType.BLUNT, Sin.GLOOM, listOf(Effect.POISE)),
+        generateSkill(DamageType.BLUNT, Sin.LUST, emptyList()),
+        dummyPassive,
+        dummySupport
+    )
+
+    private val fourthIdentity = generateIdentity(
+        "TEST IDENTITY #4",
+        IdentityDamageResistType.NORMAL,
+        IdentityDamageResistType.FATAL,
+        IdentityDamageResistType.INEFFECTIVE,
+        generateSkill(DamageType.BLUNT, Sin.ENVY, listOf(Effect.POISE)),
+        generateSkill(DamageType.PIERCE, Sin.SLOTH, listOf(Effect.POISE)),
+        generateSkill(DamageType.BLUNT, Sin.LUST, emptyList()),
+        dummyPassive,
+        dummySupport
+    )
+
+    private val testData = listOf(
+        firstIdentity,
+        secondIdentity,
+        thirdIdentity,
+        fourthIdentity
+    )
+
+    private val emptyResistArgs = FilterResistSetArg(
+        FilterDamageTypeArg.Empty,
+        FilterDamageTypeArg.Empty,
+        FilterDamageTypeArg.Empty
+    )
+
+    private val emptySkillArg = FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Empty)
+
+    @BeforeEach
+    fun prepareTestData() {
+        coEvery { repository.getAllIdentities() } answers { testData }
+    }
 
     @Test
     fun `empty filter returns full data`() {
 
         val expected = testData
-        val repository = mockk<SinnerRepository>()
-
-        coEvery { repository.getAllIdentities() } answers { testData }
-
-        val useCase = GetFilteredIdentitiesUseCase(repository)
-
-        val resistArgs = FilterResistSetArg(
-            FilterDamageTypeArg.Empty,
-            FilterDamageTypeArg.Empty,
-            FilterDamageTypeArg.Empty
-        )
 
         val skillArgs = FilterSkillsSetArg(
-            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Empty),
-            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Empty),
-            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Empty),
+            emptySkillArg,
+            emptySkillArg,
+            emptySkillArg,
         )
 
-        val filter = IdentityFilter(resistArgs, skillArgs, emptyList())
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with one slash damage type returns identities #1 & #3`() {
+        val expected = listOf(
+            firstIdentity,
+            thirdIdentity
+        ).sortedBy { it.name }
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.SLASH), FilterSinTypeArg.Empty),
+            emptySkillArg,
+            emptySkillArg,
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with slash and blunt damage types return identities #1 & #3`() {
+        val expected = listOf(
+            firstIdentity,
+            thirdIdentity
+        ).sortedBy { it.name }
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.SLASH), FilterSinTypeArg.Empty),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.BLUNT), FilterSinTypeArg.Empty),
+            emptySkillArg,
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with slash and two blunt damage types returns identity #3`() {
+        val expected = listOf(
+            thirdIdentity
+        ).sortedBy { it.name }
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.SLASH), FilterSinTypeArg.Empty),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.BLUNT), FilterSinTypeArg.Empty),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.BLUNT), FilterSinTypeArg.Empty),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with three pierce type damage returns identity #2`() {
+        val expected = listOf(
+            secondIdentity
+        ).sortedBy { it.name }
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.PIERCE), FilterSinTypeArg.Empty),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.PIERCE), FilterSinTypeArg.Empty),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.PIERCE), FilterSinTypeArg.Empty),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with pierce pierce blunt type damage returns empty list`() {
+        val expected = emptyList<Identity>()
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.PIERCE), FilterSinTypeArg.Empty),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.BLUNT), FilterSinTypeArg.Empty),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.PIERCE), FilterSinTypeArg.Empty),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with one wrath sin returns identity #1`() {
+        val expected = listOf(
+            firstIdentity
+        ).sortedBy { it.name }
+
+        val skillArgs = FilterSkillsSetArg(
+            emptySkillArg,
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.WRATH)),
+            emptySkillArg,
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with one lust sin returns identities #1 #3 #4`() {
+        val expected = listOf(
+            firstIdentity,
+            thirdIdentity,
+            fourthIdentity
+        ).sortedBy { it.name }
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.LUST)),
+            emptySkillArg,
+            emptySkillArg,
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with lust and envy sins returns identities #1 #4`() {
+        val expected = listOf(
+            firstIdentity,
+            fourthIdentity
+        ).sortedBy { it.name }
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.LUST)),
+            emptySkillArg,
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.ENVY)),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with gluttony gloom lust sins returns identity #3`() {
+        val expected = listOf(
+            thirdIdentity
+        ).sortedBy { it.name }
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.GLOOM)),
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.GLUTTONY)),
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.LUST)),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with pride lust sins returns empty list`() {
+        val expected = emptyList<Identity>()
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.PRIDE)),
+            emptySkillArg,
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.LUST)),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `strict filter with blunt_gloom returns identity #3`() {
+        val expected = listOf(thirdIdentity)
+
+        val skillArgs = FilterSkillsSetArg(
+            emptySkillArg,
+            emptySkillArg,
+            FilterSkillArg(
+                FilterDamageTypeArg.Type(DamageType.BLUNT),
+                FilterSinTypeArg.Type(Sin.GLOOM)
+            ),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `strict filter with pierce_sloth and loose slash returns emptyList`() {
+        val expected = emptyList<Identity>()
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.SLASH), FilterSinTypeArg.Empty),
+            emptySkillArg,
+            FilterSkillArg(
+                FilterDamageTypeArg.Type(DamageType.PIERCE),
+                FilterSinTypeArg.Type(Sin.SLOTH)
+            ),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `loose filter with blunt lust envy returns identities #1 #4`() {
+        val expected = listOf(firstIdentity, fourthIdentity)
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.ENVY)),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.BLUNT), FilterSinTypeArg.Empty),
+            FilterSkillArg(
+                FilterDamageTypeArg.Empty,
+                FilterSinTypeArg.Type(Sin.LUST)
+            ),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    @Test
+    fun `filter with pierce_gloom envy returns identities #1 #4`() {
+        val expected = listOf(firstIdentity, fourthIdentity)
+
+        val skillArgs = FilterSkillsSetArg(
+            FilterSkillArg(FilterDamageTypeArg.Empty, FilterSinTypeArg.Type(Sin.ENVY)),
+            FilterSkillArg(FilterDamageTypeArg.Type(DamageType.BLUNT), FilterSinTypeArg.Empty),
+            FilterSkillArg(
+                FilterDamageTypeArg.Empty,
+                FilterSinTypeArg.Type(Sin.LUST)
+            ),
+        )
+
+        return testBase(expected, skillArgs)
+    }
+
+    //TODO write more tests for combined damage/sin types cases
+    //TODO write test with resistances
+    //TODO write tests with combined resistances and skills
+    private fun testBase(expected: List<Identity>, skillArgs: FilterSkillsSetArg) {
+        val useCase = GetFilteredIdentitiesUseCase(repository)
+
+        val filter = IdentityFilter(emptyResistArgs, skillArgs, emptyList())
 
         return runTest {
-            val result = useCase.invoke(filter)
-            coVerify(atLeast = 1) { repository.getAllIdentities() }
+            val result = useCase.invoke(filter).sortedBy { it.name }
+
+            coVerify(exactly = 1) { repository.getAllIdentities() }
             confirmVerified(repository)
             assertEquals(expected, result)
         }
     }
-
-    @Test
 
     private fun generateSkill(damage: DamageType, sin: Sin, effects: List<Effect>) =
         Skill(0, "test skill", 0, damage, sin, 3, 3, 2, 2, effects)
