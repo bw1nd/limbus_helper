30b11bd75d89e76907a3df4ad6b118fcfb5e479b
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-11 23:39:16 +0200

    updated effect enum and icons

--- app/src/main/java/ua/blackwind/limbushelper/ui/util/ResourcesUtils.kt
+++ app/src/main/java/ua/blackwind/limbushelper/ui/util/ResourcesUtils.kt
@@ -41,28 +41,25 @@ fun getSinIcon(sin: Sin): Int {
 }
 
 fun getEffectIcon(effect: Effect): Int {
-    //TODO add other icons
     return when (effect) {
         Effect.BLEED -> R.drawable.bleed_ic
-        Effect.PARALYSIS -> R.drawable.bleed_ic
+        Effect.PARALYSIS -> R.drawable.paralisys_ic
         Effect.RUPTURE -> R.drawable.rupture_ic
         Effect.SINKING -> R.drawable.sink_ic
         Effect.TREMOR -> R.drawable.tremor_ic
         Effect.BURN -> R.drawable.burn_ic
-        Effect.BIND -> R.drawable.rupture_ic
-        Effect.FRAGILE -> R.drawable.rupture_ic
-        Effect.CURSE -> R.drawable.rupture_ic
-        Effect.ATTACK_UP -> R.drawable.dmg_up_ic
-        Effect.ATTACK_DOWN -> R.drawable.dmg_up_ic
-        Effect.OFFENCE_UP -> R.drawable.power_up_ic
-        Effect.OFFENCE_DOWN -> R.drawable.power_up_ic
+        Effect.BIND -> R.drawable.bind_ic
+        Effect.FRAGILE -> R.drawable.fragile_ic
+        //Effect.CURSE -> R.drawable.rupture_ic
+        Effect.ATTACK_UP -> R.drawable.att_up_ic
+        Effect.ATTACK_DOWN -> R.drawable.att_down_ic
         Effect.POISE -> R.drawable.poise_ic
-        Effect.PROTECT -> R.drawable.poise_ic
-        Effect.HASTE -> R.drawable.poise_ic
+        Effect.PROTECT -> R.drawable.protect_ic
+        Effect.HASTE -> R.drawable.haste_ic
         Effect.CHARGE -> R.drawable.charge_ic
-        Effect.AMMO -> R.drawable.burn_ic
-        Effect.DEFENSE_UP -> R.drawable.dmg_up_ic
-        Effect.DEFENSE_DOWN -> R.drawable.dmg_up_ic
-        Effect.HEAL -> R.drawable.blunt_ic
+        Effect.AMMO -> R.drawable.ammo_ic
+        Effect.DEFENSE_UP -> R.drawable.def_up_ic
+        Effect.DEFENSE_DOWN -> R.drawable.def_down_ic
+        Effect.HEAL -> R.drawable.heal_ic
     }
 }
