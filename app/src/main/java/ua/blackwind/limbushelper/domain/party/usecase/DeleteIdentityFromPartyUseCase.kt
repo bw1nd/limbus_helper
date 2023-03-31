64eb8a9184e2bd6adf1bb55ca5f76a2ddf5de4b5
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-03-31 23:22:53 +0300

    changed delete identity from party behavior

--- app/src/main/java/ua/blackwind/limbushelper/domain/party/usecase/DeleteIdentityFromPartyUseCase.kt
+++ app/src/main/java/ua/blackwind/limbushelper/domain/party/usecase/DeleteIdentityFromPartyUseCase.kt
@@ -9,24 +9,6 @@ class DeleteIdentityFromPartyUseCase @Inject constructor(
     private val repository: IPartyRepository
 ) {
     suspend operator fun invoke(identity: Identity, party: Party) {
-        val activeIdentity =
-            repository.getActiveIdentityIdForPartyAndSinner(party.id, identity.sinnerId)
-        val otherIdentitiesForThisSinner =
-            party.identityList.filter { it.identity.sinnerId == identity.sinnerId && it.identity.id != identity.id }
-
-        if (activeIdentity == identity.id) {
-            repository.changeSinnerActiveIdentityForParty(
-                partyId = party.id,
-                sinnerId = identity.sinnerId,
-                identityId = if (otherIdentitiesForThisSinner.isNotEmpty())
-                    otherIdentitiesForThisSinner.first().identity.id else NO_ACTIVE_SINNER_IDENTITY_IN_PARTY
-            )
-        }
-
         repository.deleteIdentityFromParty(party.id, identity)
-    }
-
-    companion object {
-        private const val NO_ACTIVE_SINNER_IDENTITY_IN_PARTY = 0
     }
 }
