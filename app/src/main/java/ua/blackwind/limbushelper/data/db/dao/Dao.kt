db59a9afdf73a625de0946b0ebfab1eb8f8bb631
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-04-03 14:10:06 +0300

    added methods to EgoDao, added EgoDao to main Dao

--- app/src/main/java/ua/blackwind/limbushelper/data/db/dao/Dao.kt
+++ app/src/main/java/ua/blackwind/limbushelper/data/db/dao/Dao.kt
@@ -3,4 +3,4 @@ package ua.blackwind.limbushelper.data.db.dao
 import androidx.room.Dao
 
 @Dao
-interface Dao: SinnerDao, IdentityDao, SkillDao, PartyDao
+interface Dao: SinnerDao, IdentityDao, EgoDao, SkillDao, PartyDao
