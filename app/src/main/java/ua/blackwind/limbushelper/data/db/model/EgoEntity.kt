07a9971895afa6b7563193bc3db8bd4beab11184
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-04-04 20:55:22 +0300

    changed way converters for ego sinResistances and ego resourceCost code data

--- app/src/main/java/ua/blackwind/limbushelper/data/db/model/EgoEntity.kt
+++ app/src/main/java/ua/blackwind/limbushelper/data/db/model/EgoEntity.kt
@@ -1,6 +1,5 @@
 package ua.blackwind.limbushelper.data.db.model
 
-import android.database.sqlite.SQLiteDatabaseCorruptException
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverter
@@ -10,9 +9,9 @@ import ua.blackwind.limbushelper.domain.common.Sin
 import ua.blackwind.limbushelper.domain.sinner.model.Ego
 import ua.blackwind.limbushelper.domain.sinner.model.Passive
 import ua.blackwind.limbushelper.domain.sinner.model.Skill
-import java.util.*
 
-private const val MAP_CONVERTER_DIVIDER = "_"
+private const val MAP_CONVERTER_PAIR_DIVIDER = "_"
+private const val MAP_CONVERTER_ITEM_DIVIDER = ","
 
 @Entity(tableName = "ego")
 data class EgoEntity(
@@ -54,47 +53,40 @@ suspend fun EgoEntity.toEgo(
 class RoomTypeConverters {
     @TypeConverter
     fun fromResourceCostMap(value: Map<Sin, Int>): String {
-        val sortedMap = TreeMap(value)
-        return sortedMap.keys.joinToString(separator = ",").plus(MAP_CONVERTER_DIVIDER)
-            .plus(sortedMap.values.joinToString(separator = ","))
+        throw NotImplementedError("Normally this method should never be used")
     }
 
     @TypeConverter
     fun toResourceCostMap(value: String): Map<Sin, Int> {
-        return value.split(MAP_CONVERTER_DIVIDER).run {
-            val keys = getOrNull(0)?.split(",")?.map { it }
-            val values = getOrNull(1)?.split(",")?.map { it }
+        return value.split(MAP_CONVERTER_ITEM_DIVIDER).let { list ->
+            try {
+                list.associate { string ->
+                    val split = string.split(MAP_CONVERTER_PAIR_DIVIDER)
 
-            val res = hashMapOf<Sin, Int>()
-            keys?.forEachIndexed { index, s ->
-                res[Sin.valueOf(s)] = values?.getOrNull(index)?.toInt()
-                    ?: throw SQLiteDatabaseCorruptException("Database data for Ego ResourceCost has been corrupted.")
+                    Sin.valueOf(split.first()) to split.last().toInt()
+                }
+            } catch (e: Exception) {
+                throw Exception("Database Ego data corrupted for: $value", e)
             }
-            res
         }
     }
 
     @TypeConverter
     fun fromSinResistancesMap(value: Map<Sin, EgoSinResistType>): String {
-        val sortedMap = TreeMap(value)
-        return sortedMap.keys.joinToString(separator = ",").plus(MAP_CONVERTER_DIVIDER)
-            .plus(sortedMap.values.joinToString(separator = ","))
+        throw NotImplementedError("Normally this method should never be used")
     }
 
     @TypeConverter
     fun toSinResistancesMap(value: String): Map<Sin, EgoSinResistType> {
-        return value.split(MAP_CONVERTER_DIVIDER).run {
-            val keys = getOrNull(0)?.split(",")?.map { it }
-            val values = getOrNull(1)?.split(",")?.map { it }
-
-            val res = hashMapOf<Sin, EgoSinResistType>()
-            keys?.forEachIndexed { index, s ->
-                res[Sin.valueOf(s)] = EgoSinResistType.valueOf(
-                    values?.getOrNull(index)
-                        ?: throw SQLiteDatabaseCorruptException("Database data for Ego SinResistances has been corrupted.")
-                )
+        return value.split(MAP_CONVERTER_ITEM_DIVIDER).let { list ->
+            try {
+                list.associate { string ->
+                    val split = string.split(MAP_CONVERTER_PAIR_DIVIDER)
+                    Sin.valueOf(split.first()) to EgoSinResistType.valueOf(split.last())
+                }
+            } catch (e: Exception) {
+                throw Exception("Database Ego data corrupted for: $value", e)
             }
-            res
         }
     }
 }
