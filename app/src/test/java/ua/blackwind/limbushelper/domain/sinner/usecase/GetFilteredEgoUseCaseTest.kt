ea7bd38bb7591143263588b51b608687c301dc8e
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-04-06 07:52:00 +0300

    added filter ego use case test

---
+++ app/src/test/java/ua/blackwind/limbushelper/domain/sinner/usecase/GetFilteredEgoUseCaseTest.kt
@@ -0,0 +1,8 @@
+package ua.blackwind.limbushelper.domain.sinner.usecase
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class GetFilteredEgoUseCaseTest {
+    //TODO write tests for this use case
+}
