fa343ed640beb73439e08e91241e4b5219954eb5
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-04-13 18:05:30 +0300

    added `DefenceSkill` model and `DefenceSkillType` to domain.

---
+++ app/src/main/java/ua/blackwind/limbushelper/domain/sinner/model/DefenceSkill.kt
@@ -0,0 +1,20 @@
+package ua.blackwind.limbushelper.domain.sinner.model
+
+import ua.blackwind.limbushelper.domain.common.Sin
+
+data class DefenceSkill(
+    val id: Int,
+    val name: String,
+    val type: DefenceSkillType,
+    val skillPower: Int,
+    val coinPower: Int,
+    val coinCount: Int,
+    val sin: Sin?,
+    val defenceLevel: Int
+)
+
+enum class DefenceSkillType {
+    Guard,
+    Evade,
+    Counter
+}
