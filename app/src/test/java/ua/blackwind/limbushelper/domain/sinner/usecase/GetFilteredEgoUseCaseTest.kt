9fb1b7bc265ae6bae17c6cb31fa3653803ac8f93
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-04-23 07:13:29 +0300

    moved unit test data to separate `TestEntitiesCreator` class.

--- app/src/test/java/ua/blackwind/limbushelper/domain/sinner/usecase/GetFilteredEgoUseCaseTest.kt
+++ app/src/test/java/ua/blackwind/limbushelper/domain/sinner/usecase/GetFilteredEgoUseCaseTest.kt
@@ -10,54 +10,16 @@ import ua.blackwind.limbushelper.data.SinnerRepository
 import ua.blackwind.limbushelper.domain.common.*
 import ua.blackwind.limbushelper.domain.filter.*
 import ua.blackwind.limbushelper.domain.sinner.model.*
+import util.TestEntitiesCreator
 
-class GetFilteredEgoUseCaseTest {
+internal class GetFilteredEgoUseCaseTest {
 
     private val repository = mockk<SinnerRepository>()
+    private val entityGenerator = TestEntitiesCreator()
 
-    private val firstEgo = generateEgo(
-        name = "TEST EGO #1",
-        sinnerId = 2,
-        riskLevel = RiskLevel.ZAYIN,
-        awakeningSkill = generateEgoSkill(DamageType.BLUNT, Sin.PRIDE, listOf(Effect.ATT_UP)),
-        resourceCost = mapOf(Sin.LUST to 2, Sin.PRIDE to 2),
-        sinResistances = mapOf(
-            Sin.LUST to EgoSinResistType.FATAL,
-            Sin.GLUTTONY to EgoSinResistType.FATAL,
-            Sin.PRIDE to EgoSinResistType.ENDURE
-        )
-    )
-
-    private val secondEgo = generateEgo(
-        "TEST EGO #2",
-        4,
-        RiskLevel.HE,
-        generateEgoSkill(
-            DamageType.SLASH,
-            Sin.WRATH,
-            effects = listOf(Effect.BURN)
-        ),
-        mapOf(Sin.WRATH to 4, Sin.PRIDE to 2),
-        mapOf(
-            Sin.WRATH to EgoSinResistType.INEFF,
-            Sin.SLOTH to EgoSinResistType.FATAL,
-            Sin.GLOOM to EgoSinResistType.FATAL,
-            Sin.ENVY to EgoSinResistType.ENDURE
-        )
-    )
-
-    private val thirdEgo = generateEgo(
-        "TEST EGO #3",
-        2,
-        RiskLevel.TETH,
-        generateEgoSkill(DamageType.SLASH, Sin.ENVY, listOf(Effect.FRAGILE)),
-        mapOf(Sin.ENVY to 6),
-        mapOf(
-            Sin.WRATH to EgoSinResistType.FATAL,
-            Sin.LUST to EgoSinResistType.FATAL,
-            Sin.ENVY to EgoSinResistType.INEFF
-        )
-    )
+    private val firstEgo = entityGenerator.firstEgo
+    private val secondEgo = entityGenerator.secondEgo
+    private val thirdEgo = entityGenerator.thirdEgo
 
     private val testData = listOf(firstEgo, secondEgo, thirdEgo)
 
@@ -245,38 +207,4 @@ class GetFilteredEgoUseCaseTest {
             assertEquals(expected, result)
         }
     }
-
-    private fun generateEgo(
-        name: String,
-        sinnerId: Int,
-        riskLevel: RiskLevel,
-        awakeningSkill: EgoSkill,
-        resourceCost: Map<Sin, Int>,
-        sinResistances: Map<Sin, EgoSinResistType>
-    ) = Ego(
-        id = 0,
-        name = name,
-        sinnerId = sinnerId,
-        risk = riskLevel,
-        awakeningSkill = awakeningSkill,
-        corrosionSkill = null,
-        passive = Passive(0, 0, SinCost(emptyList()), ""),
-        resourceCost = resourceCost,
-        sanityCost = 0,
-        sinResistances = sinResistances,
-        imageUrl = ""
-    )
-
-    private fun generateEgoSkill(dmgType: DamageType, sin: Sin, effects: List<Effect>) = EgoSkill(
-        id = 0,
-        name = "",
-        dmgType = dmgType,
-        sin = sin,
-        offense = 0,
-        basePower = 0,
-        coinPower = 0,
-        coinCount = 0,
-        sanityCost = 0,
-        effects = effects
-    )
 }
--- app/src/test/java/ua/blackwind/limbushelper/domain/sinner/usecase/GetFilteredIdentitiesUseCaseTest.kt
+++ app/src/test/java/ua/blackwind/limbushelper/domain/sinner/usecase/GetFilteredIdentitiesUseCaseTest.kt
@@ -10,94 +10,17 @@ import ua.blackwind.limbushelper.data.SinnerRepository
 import ua.blackwind.limbushelper.domain.common.*
 import ua.blackwind.limbushelper.domain.filter.*
 import ua.blackwind.limbushelper.domain.sinner.model.*
+import util.TestEntitiesCreator
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class GetFilteredIdentitiesUseCaseTest {
-
-    private val dummyPassive = mockk<Passive>()
-
-    private val dummySupport = mockk<Support>()
-
+internal class GetFilteredIdentitiesUseCaseTest {
     private val repository = mockk<SinnerRepository>()
+    private val entityGenerator = TestEntitiesCreator()
 
-    private val firstIdentity = generateIdentity(
-        name = "TEST IDENTITY #1",
-        sinnerId = 1,
-        slashResist = IdentityDamageResistType.INEFF,
-        pierceResist = IdentityDamageResistType.NORMAL,
-        bluntResist = IdentityDamageResistType.FATAL,
-        firstSkill = generateSkill(DamageType.SLASH, Sin.LUST, listOf(Effect.BURN)),
-        secondSkill = generateSkill(
-            DamageType.SLASH,
-            Sin.ENVY,
-            listOf(Effect.BLEED, Effect.PARALYSIS)
-        ),
-        thirdSkill = generateSkill(DamageType.BLUNT, Sin.WRATH, emptyList()),
-        defenseSkill = DefenseSkill(0, "Guard", DefenseSkillType.GUARD, 0, 0, 0, null),
-        passive = dummyPassive,
-        support = dummySupport
-    )
-    private val secondIdentity = generateIdentity(
-        name = "TEST IDENTITY #2",
-        sinnerId = 2,
-        slashResist = IdentityDamageResistType.NORMAL,
-        pierceResist = IdentityDamageResistType.INEFF,
-        bluntResist = IdentityDamageResistType.FATAL,
-        firstSkill = generateSkill(DamageType.PIERCE, Sin.PRIDE, listOf(Effect.POISE)),
-        secondSkill = generateSkill(
-            DamageType.PIERCE,
-            Sin.SLOTH,
-            listOf(Effect.POISE, Effect.FRAGILE)
-        ),
-        thirdSkill = generateSkill(DamageType.PIERCE, Sin.ENVY, emptyList()),
-        defenseSkill = DefenseSkill(
-            0,
-            "Counter Wrath",
-            DefenseSkillType.COUNTER,
-            0,
-            0,
-            0,
-            Sin.WRATH
-        ),
-        passive = dummyPassive,
-        support = dummySupport
-    )
-
-    private val thirdIdentity = generateIdentity(
-        name = "TEST IDENTITY #3",
-        sinnerId = 3,
-        slashResist = IdentityDamageResistType.NORMAL,
-        pierceResist = IdentityDamageResistType.INEFF,
-        bluntResist = IdentityDamageResistType.FATAL,
-        firstSkill = generateSkill(DamageType.SLASH, Sin.GLUTTONY, listOf(Effect.BLEED)),
-        secondSkill = generateSkill(DamageType.BLUNT, Sin.GLOOM, listOf(Effect.POISE)),
-        thirdSkill = generateSkill(DamageType.BLUNT, Sin.LUST, emptyList()),
-        defenseSkill = DefenseSkill(
-            0,
-            "Counter Wrath",
-            DefenseSkillType.COUNTER,
-            0,
-            0,
-            0,
-            Sin.WRATH
-        ),
-        passive = dummyPassive,
-        support = dummySupport
-    )
-
-    private val fourthIdentity = generateIdentity(
-        name = "TEST IDENTITY #4",
-        sinnerId = 1,
-        slashResist = IdentityDamageResistType.INEFF,
-        pierceResist = IdentityDamageResistType.FATAL,
-        bluntResist = IdentityDamageResistType.NORMAL,
-        firstSkill = generateSkill(DamageType.BLUNT, Sin.ENVY, listOf(Effect.POISE)),
-        secondSkill = generateSkill(DamageType.PIERCE, Sin.SLOTH, listOf(Effect.POISE)),
-        thirdSkill = generateSkill(DamageType.BLUNT, Sin.LUST, emptyList()),
-        defenseSkill = DefenseSkill(0, "Counter Lust", DefenseSkillType.COUNTER, 0, 0, 0, Sin.LUST),
-        passive = dummyPassive,
-        support = dummySupport
-    )
+    private val firstIdentity = entityGenerator.firstIdentity
+    private val secondIdentity = entityGenerator.secondIdentity
+    private val thirdIdentity = entityGenerator.thirdIdentity
+    private val fourthIdentity = entityGenerator.fourthIdentity
 
     private val testData = listOf(
         firstIdentity,
@@ -542,40 +465,4 @@ class GetFilteredIdentitiesUseCaseTest {
             assertEquals(expected, result)
         }
     }
-
-    private fun generateSkill(damage: DamageType, sin: Sin, effects: List<Effect>) =
-        Skill(0, "test skill", damage, sin, 3, 3, 2, 2, effects)
-
-    private fun generateIdentity(
-        name: String,
-        sinnerId: Int,
-        slashResist: IdentityDamageResistType,
-        pierceResist: IdentityDamageResistType,
-        bluntResist: IdentityDamageResistType,
-        firstSkill: Skill,
-        secondSkill: Skill,
-        thirdSkill: Skill,
-        defenseSkill: DefenseSkill,
-        passive: Passive,
-        support: Support
-    ) = Identity(
-        id = 0,
-        name = name,
-        sinnerId = sinnerId,
-        rarity = 0,
-        slashRes = slashResist,
-        pierceRes = pierceResist,
-        bluntRes = bluntResist,
-        hp = 100,
-        defense = 30,
-        offense = 30,
-        speed = 2 to 5,
-        firstSkill = firstSkill,
-        secondSkill = secondSkill,
-        thirdSkill = thirdSkill,
-        defenseSkill = defenseSkill,
-        passive = passive,
-        support = support,
-        imageUrl = ""
-    )
 }
